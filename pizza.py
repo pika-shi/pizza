cb0d2e9b17e8faa9ba56b41354d16af4fbbda18a
竹村 光 <takemura@dl.kuis.kyoto-u.ac.jp>
2014-02-28 21:04:31 +0900

    intaractive mode 追加

--- pizza.py
+++ pizza.py
@@ -13,21 +13,42 @@ def main():
 
     args = parse(description)
 
-    if args.menu == True:
-        print description
+    if args.interactive == True:
+        flag = True
 
-    if not (len(args.kind) == len(args.size) == len(args.num)):
+        while flag:
+            print 'input pizza number(1-26)'
+            args.kind.append(raw_input())
+            print 'input size of pizza (35 or 45)'
+            args.size.append(raw_input())
+            print 'input number of pizza(1-)'
+            args.num.append(raw_input())
+            print 'Do you order another pizza?(y/n)'
+            if raw_input() != 'y':
+                flag = False
+
+        print args.kind[0]
+        print args.size
+        for i in range(len(args.kind)):
+            print 'Order' + str(i+1) + ':PizzaNumber ' + str(args.kind[i]) + ' SizeOfPizza '+ str(args.size[i]) + ' NumberOfPizza ' + str(args.num[i])
+
+        print 'Are you sure this order?(y/n)'
+        if raw_input() != 'y':
+            sys.exit()
+
+    if not (len(args.kind) == len(args.size) == len(args.num) != 0):
         print 'Error: You have to equalize num of arguments(k, s, and n).'
         sys.exit()
 
     contact_dict = get_contact_dict()
 
     validate_contact(contact_dict)
-
+    """
     kind_list = [pizza_dict[kind]['name'] for kind in args.kind]
     TwilioCall().order_pizza({'name':contact_dict[0]['name'], 'tel':contact_dict[0]['tel'],
                               'dlt':contact_dict[0]['delivery_limit_time'],
                               'kind_list':kind_list, 'size_list':args.size, 'num_list':args.num})
+    """
 
 def get_pizza_dict():
     pizza_dict = {}
@@ -44,12 +65,12 @@ def parse(description):
         description=description)
 
     parser.add_argument('-k', '--kind', type=int, choices=xrange(1,27),
-                        dest='kind', default=[3], nargs='+', help='kind of pizza')
+                        dest='kind', default=[], nargs='+', help='kind of pizza')
     parser.add_argument('-s', '--size', type=int, choices=[35, 45], dest='size',
-                        default=[45], nargs='+', help='size of pizza')
-    parser.add_argument('-n', '--num', type=int, dest='num', default=[1],
+                        default=[], nargs='+', help='size of pizza')
+    parser.add_argument('-n', '--num', type=int, dest='num', default=[],
                         nargs='+', help='number of pizza')
-    parser.add_argument('-m','--menu',dest='menu',action='store_true', help='shows the list of pizzas')
+    parser.add_argument('-i','--interactive',dest='interactive',action='store_true', help='interactive order mode')
 
     return parser.parse_args()
 
@@ -73,7 +94,7 @@ def get_contact_dict():
 
 def validate_contact(contact_dict):
     for v in contact_dict.itervalues():
-        phone_pattern = re.compile(r'^(\d{3})(-)?(\d{3,4})(-)?(\d{4})$')
+        phone_pattern = re.compile(r'^(\d{3})(\d{3,4})(\d{4})$')
         if not(phone_pattern.match(v['tel'])):
             print 'Error: Invalid Phone Number.'
             sys.exit()
